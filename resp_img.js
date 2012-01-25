462141a7dd2cb9b22d4ba1041e14602f1966d62d
Peter Droogmans <peter@attiks.com>
2012-01-25 20:40:53 +0100

    act on resize

--- resp_img.js
+++ resp_img.js
@@ -1,44 +1,52 @@
 (function ($) {
-  var windowWidth = $(window).width();
   Drupal.behaviors.respImg = {
     attach: function (context) {
-      if ($.cookie("Drupal.visitor.ri_cookie") === null) {
-        var current_suffix = Drupal.settings.respImg.current_suffix;
-        if (Drupal.settings.respImg.current_suffix === false) {
-          current_suffix = Drupal.settings.respImg.default_suffix;
-        }
-        
-        var suffix = '';
-        $.each(Drupal.settings.respImg.suffixes, function(index, value){
-          if (value <= screen.availWidth) {
-            suffix = index;
-            //return false in jQuery.each() equals 'break;' in a traditional loop
-            return false;
-          }
-        });
-        
-        if (suffix !== '' && suffix !== current_suffix) {
-          $('img').each(function() {
-            var img = $(this);
-            var src = img.attr('src').replace(current_suffix, suffix);
-            img.attr('src', src);
-          });
-          $('a.colorbox').each(function() {
-            var a = $(this);
-            var href = a.attr('href').replace(current_suffix, suffix);
-            a.attr('href', href);
-          });
-        }
-      }
+      Drupal.respImg_processSuffixes();
+      $(window).resize(function() {
+        Drupal.respImg_processSuffixes();
+      });
 
-      $.cookie(
-        "Drupal.visitor.ri_cookie",
-        screen.availWidth,
-        {
-          path: Drupal.settings.basePath,
-          expires: 1
-        }
-      );
     }
   }
+  
+  Drupal.respImg_processSuffixes = function() {
+    if (true || Drupal.settings.respImg.current_suffix === false || $.cookie("Drupal.visitor.ri_cookie") === null) {
+      var current_suffix = Drupal.settings.respImg.current_suffix;
+      if (Drupal.settings.respImg.current_suffix === false) {
+        current_suffix = Drupal.settings.respImg.default_suffix;
+      }
+      
+      var suffix = '';
+      $.each(Drupal.settings.respImg.suffixes, function(index, value){
+        if (value <= $(window).width()) {
+          suffix = index;
+          //return false in jQuery.each() equals 'break;' in a traditional loop
+          return false;
+        }
+      });
+      
+      if (suffix !== '' && suffix !== current_suffix) {
+        $('img').each(function() {
+          var img = $(this);
+          var src = img.attr('src').replace(current_suffix, suffix);
+          img.attr('src', src);
+        });
+        $('a.colorbox').each(function() {
+          var a = $(this);
+          var href = a.attr('href').replace(current_suffix, suffix);
+          a.attr('href', href);
+        });
+        Drupal.settings.respImg.current_suffix = suffix;
+      }
+    }
+    
+    $.cookie(
+      "Drupal.visitor.ri_cookie",
+      $(window).width(),
+      {
+        path: Drupal.settings.basePath,
+        expires: 1
+      }
+    );
+  }  
 } (jQuery));
