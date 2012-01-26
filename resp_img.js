56dd574586dfd63135ce07d62819ac3b4e3d334f
Peter Droogmans <peter@attiks.com>
2012-01-26 15:22:37 +0100

    added forceredirect and forceresize options

--- resp_img.js
+++ resp_img.js
@@ -3,9 +3,10 @@
     attach: function (context) {
       Drupal.respImg_processSuffixes();
       $(window).resize(function() {
-        Drupal.respImg_processSuffixes();
+        if ($(window).width() > 10) {
+          Drupal.respImg_processSuffixes();
+        }
       });
-
     }
   }
   
@@ -23,13 +24,14 @@
             expires: 1
           }
         );
-        return false;
+        return false; // exits .each
       }
     });
     return suffix;
   }
   
   Drupal.respImg_processSuffixes = function() {
+    // Redirect user if needed / wanted
     if (Drupal.settings.respImg.current_suffix === false && Drupal.settings.respImg.forceRedirect) {
       // Make sure browser accepts cookies
       $.cookie('respimg_test', 'ok');
@@ -40,49 +42,53 @@
       }
     }
     
-    if (true || Drupal.settings.respImg.current_suffix === false || $.cookie("respimg") === null) {
-      var current_suffix = Drupal.settings.respImg.current_suffix;
-      if (Drupal.settings.respImg.current_suffix === false) {
-        current_suffix = Drupal.settings.respImg.default_suffix;
-      }
+    // get currently used suffix, or default
+    var current_suffix = Drupal.settings.respImg.current_suffix;
+    if (Drupal.settings.respImg.current_suffix === false) {
+      current_suffix = Drupal.settings.respImg.default_suffix;
+    }
+    
+    // get optimal suffix
+    var suffix = Drupal.respImg_getOptimalSuffix();
+    
+    if (Drupal.settings.respImg.reloadOnResize && suffix !== '' && suffix !== current_suffix) {
+      location.reload(true);
+    }
+    
+    if (Drupal.settings.respImg.forceResize && suffix !== '' && suffix !== current_suffix) {
+      // support for images
+      $('img').each(function() {
+        var img = $(this);
+        var src = img.attr('src').replace(current_suffix, suffix);
+        img.attr('src', src);
+        img.removeAttr('width');
+        img.removeAttr('height');
+      });
       
-      var suffix = Drupal.respImg_getOptimalSuffix();
+      // support for colorbox links
+      $('a.colorbox').each(function() {
+        var a = $(this);
+        var href = a.attr('href').replace(current_suffix, suffix);
+        a.attr('href', href);
+      });
       
-      if (Drupal.settings.respImg.forceResize && suffix !== '' && suffix !== current_suffix) {
-        // support for images
-        $('img').each(function() {
-          var img = $(this);
-          var src = img.attr('src').replace(current_suffix, suffix);
-          img.attr('src', src);
-          img.removeAttr('width');
-          img.removeAttr('height');
-        });
-        
-        // support for colorbox links
-        $('a.colorbox').each(function() {
-          var a = $(this);
-          var href = a.attr('href').replace(current_suffix, suffix);
-          a.attr('href', href);
-        });
-        
-        // support for field_slideshow (kind of)
-        if (typeof(Drupal.behaviors.field_slideshow) == "object") {
-          $('div.field-slideshow-processed')
-            .cycle('destroy')
-            .removeClass('field-slideshow-processed')
-            .css('width', '')
-            .css('height', '')
-            .css('padding-bottom', '')
-            .each(function() {
-              var $field = $(this);
-              var $child = $field.children('div.field-slideshow-slide').first();
-              console.log($child);
-              console.log($child.css('width'));
-              $field.css('width', $child.css('width'));
-            });
-          $('div.field-slideshow-slide').css('width', '').css('height', '');
-          Drupal.behaviors.field_slideshow.attach();
-        }
+      // support for field_slideshow (kind of)
+      if (typeof(Drupal.behaviors.field_slideshow) == "object") {
+        $('div.field-slideshow-processed')
+          .cycle('destroy')
+          .removeClass('field-slideshow-processed')
+          .css('width', '')
+          .css('height', '')
+          .css('padding-bottom', '')
+          .each(function() {
+            var $field = $(this);
+            var $child = $field.children('div.field-slideshow-slide').first();
+            console.log($child);
+            console.log($child.css('width'));
+            $field.css('width', $child.css('width'));
+          });
+        $('div.field-slideshow-slide').css('width', '').css('height', '');
+        Drupal.behaviors.field_slideshow.attach();
       }
       
       // store last used suffix
